5f51f67799cc954528ad7e2038a05c43f733acda
Zsombor Nyikus <zsombor.nyikus@stud.uni-corvinus.hu>
2020-12-12 13:41:20 +0100

    commit

---
+++ IRF_Project_VLM0UR/IRF_Project_VLM0UR/Orszagok.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRF_Project_VLM0UR
+{
+    class Orszagok
+    {
+        private string _honap;
+        public string Datum { get; set; }
+        public string Honap
+        {
+            get { return _honap; }
+            set
+            {
+                if ((value[6].ToString() == "1") && (value[7].ToString() == "0")) //amúgy 5. és 6. elem lenne de ez a fos "-val kezdi
+                {
+                    _honap = "Október";
+                }
+                if ((value[6].ToString() == "1") && (value[7].ToString() == "1"))
+                {
+                    _honap = "November";
+                }
+            }
+
+        }
+        public int Esetszam { get; set; }
+    }
+}
