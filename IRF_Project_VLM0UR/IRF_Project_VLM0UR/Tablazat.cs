85526b0abaee2405829883a21335f9d6cda44975
Zsombor Nyikus <zsombor.nyikus@stud.uni-corvinus.hu>
2020-12-12 13:55:01 +0100

    commit

---
+++ IRF_Project_VLM0UR/IRF_Project_VLM0UR/Tablazat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IRF_Project_VLM0UR
+{
+    class Tablazat : Label
+    {
+        public Tablazat()
+        {
+            Height = 20;
+            Width = 90;
+            BackColor = Color.LightBlue;
+            TextAlign = ContentAlignment.MiddleCenter;
+            Font = new Font("Microsoft Sans Serif", 7.8F, FontStyle.Regular);
+            
+        }
+    }
+}
