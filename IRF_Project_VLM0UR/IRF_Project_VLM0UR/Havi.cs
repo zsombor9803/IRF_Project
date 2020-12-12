ebd9827c26cb69da14776c9599e17bda61f54323
Zsombor Nyikus <zsombor.nyikus@stud.uni-corvinus.hu>
2020-12-12 13:32:02 +0100

    commit

---
+++ IRF_Project_VLM0UR/IRF_Project_VLM0UR/Havi.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRF_Project_VLM0UR
+{
+    class Havi
+    {
+        public string Honap { get; set; }
+        public int Magyarorszag { get; set; }
+        public int Franciaorszag { get; set; }
+        public int Nemetorszag { get; set; }
+        public int Spanyolorszag { get; set; }
+        public int Olaszorszag { get; set; }
+        public int Horvatorszag { get; set; }
+        public int Oroszorszag { get; set; }
+        public int Japan { get; set; }
+        public int USA { get; set; }
+    }
+}
