9fdc357c81afb0f4c3d5b183aef14ca28ab4de28
Zsombor Nyikus <zsombor.nyikus@stud.uni-corvinus.hu>
2020-12-12 14:31:00 +0100

    commit

--- IRF_Project_VLM0UR/IRF_Project_VLM0UR/Multheti.cs
+++ IRF_Project_VLM0UR/IRF_Project_VLM0UR/Multheti.cs
@@ -20,6 +20,7 @@ namespace IRF_Project_VLM0UR
         {
             InitializeComponent();
             Beolvasas();
+            Tablazatbairas(szam);
         }
 
         void Beolvasas()
@@ -48,5 +49,67 @@ namespace IRF_Project_VLM0UR
             }
             szam--;
         }
+
+        void Tablazatbairas(int sor)
+        {
+
+            for (int i = 0; i < szam+1; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Tablazat t = new Tablazat();
+                    t.Top = 40 + i * (t.Height + 5);
+                    t.Left = 40 + j * (t.Width + 5);
+
+                    switch (j)
+                    {
+                        case 0:
+                            t.Text = _napi[i].Orszag;
+                            break;
+                        case 1:
+                            t.Text = (_napi[i].Hetfo).ToString();
+                            break;
+                        case 2:
+                            t.Text = (_napi[i].Kedd).ToString();
+                            break;
+                        case 3:
+                            t.Text = (_napi[i].Szerda).ToString();
+                            break;
+                        case 4:
+                            t.Text = (_napi[i].Csut).ToString();
+                            break;
+                        case 5:
+                            t.Text = (_napi[i].Pentek).ToString();
+                            break;
+                        case 6:
+                            t.Text = (_napi[i].Szombat).ToString();
+                            break;
+                        case 7:
+                            t.Text = (_napi[i].Vasarnap).ToString();
+                            break;
+                        case 8:
+                            t.Text = (_napi[i].Heti).ToString();
+                            break;
+                        case 9:
+                            if (_napi[i].Besorolas == 1)
+                            {
+                                t.Text = "Veszélyes";
+                            }
+                            if (_napi[i].Besorolas == 2)
+                            {
+                                t.Text = "Nem veszélyes";
+                            }
+                            if (_napi[i].Besorolas == 3)
+                            {
+                                t.Text = "Közepes";
+                            }
+                            break;
+                    }
+                    Controls.Add(t);
+                }
+            }
+        }
+
+
     }
 }
